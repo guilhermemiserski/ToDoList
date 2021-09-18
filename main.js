653e7a2ddf3d5d7e16638e3790b0025cce016fc1
Guilherme Miserski <guilhermemiserski@hotmail.com>
2021-09-18 16:47:11 -0300

    acrescentando deletar tarefa

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
-function adicionar(){
-    let atividade = document.getElementById("atividade").value;
-    let listaAtividades  = document.getElementById("listaatividades").innerHTML;
-    listaAtividades = listaAtividades + "<li>" + atividade + "<li/>";
-    document.getElementById("listaatividades").innerHTML = listaAtividades;
+const elementoLista = document.querySelector('ul');
+const elementoInput = document.querySelector('input');
+const elementoButton = document.querySelector('button');
+
+const tarefas = [];
+
+function mostraTarefas(){
+
+    elementoLista.innerHTML = '';
+    for(tarefa of tarefas){
+        const elementoTarefa = document.createElement('li');
+        const textoTarefa = document.createTextNode(tarefa);
+
+        const elementoLink = document.createElement('a');
+        const pos = tarefas.indexOf(tarefa);
+
+        elementoLink.setAttribute('href', '#')
+        elementoLink.setAttribute('onclick', `excluiTarefa(${pos})`);
+        const textoLink = document.createTextNode('X');
+        elementoLink.appendChild(textoLink);
+
+        elementoTarefa.appendChild(textoTarefa);
+        elementoLista.appendChild(elementoTarefa);
+        elementoTarefa.appendChild(elementoLink);
+    }
 }
+
+function adicionarTarefa(){
+    const textoTarefa = elementoInput.value
+    tarefas.push(textoTarefa);
+    elementoInput.value = '';
+
+    mostraTarefas();
+}
+
+function excluiTarefa(pos){
+    tarefas.splice(pos,1);
+    mostraTarefas();
+}
+
+elementoButton.setAttribute('onclick', 'adicionarTarefa()');
